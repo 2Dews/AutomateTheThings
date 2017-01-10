483768724c2942d5066f53d89db6d1d461f379f8
Gus <gusmartinka@gmail.com>
2017-01-09 19:57:54 -0700

    Creating FreshDevMachine.ps1
    First draft of Chocolatey script for dev environment setup.

---
+++ FreshDevMachine.ps1
@@ -0,0 +1,25 @@
+# Dev apps to install
+
+# Visual studio stuff
+cinstm visualstudio2015enterprise
+cinstm visualstudiocode
+
+# Visual studio add-ins and stuff
+cinstm Nunit 
+cinstm resharper
+
+# databases and tools
+
+# browsers
+cinstm GoogleChrome
+
+# Other tools
+cinstm git
+cinstm gitextensions
+cinstm notepadplusplus
+cinstm 7zip
+cinstm nodejs.install
+cinstm fiddler
+cinstm curl
+cinstm linqpad4
+
